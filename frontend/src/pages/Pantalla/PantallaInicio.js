07dbb9d2a2f09e0f9e808f570328363c2a30b203
ImLuu <lmc018@alumnos.ucn.cl>
2022-09-19 19:22:39 -0300

    Posible Termino de la pantalla principal
    casi completa , falta funcionamiento de los botones
    y detalles minimos

--- frontend/src/pages/Pantalla/PantallaInicio.js
+++ frontend/src/pages/Pantalla/PantallaInicio.js
@@ -6,32 +6,34 @@ import background from "../../components/logo.jpg";
 export const PantallaInicio = () => {
 
   return (
-   
-  <div className='row mt-0'>
-    <div className='container'>
-        <div className='fondo'>
+   <>
+      <body>
+        <div className='container'>
+            <div className='fondo' >
+              <button className='btn btn-secundary' style={{float:'right'}}>
+                    Acerca de
+              </button>
 
-          <button className='btn btn-secundary'style={{float:'right' }}>
-                Acerca de
-          </button>
+              <button className='btn btn-secundary' style={{float:'right'}}>
+                  Agendar
+              </button>
 
-          <button className='btn btn-secundary'style={{float:'right' }} >
-              Agendar
-          </button>
+              <button className='btn btn-secundary' style={{float:'right'}}>
+                  Inicio Sesion 
+              </button>
+              <img src={`${background}`} 
+                alt="" >
+              </img>
+             
 
-          <button className='btn btn-secundary' style={{float:'right' }}>
-              Inicio Sesion 
-          </button>
-          
-          <img src={`${background}`} 
-              alt="">
-          </img>
+              <div className='img2'></div>
+              <h2> CLINICA VETERINARIA</h2>
+              <h1>PUERTAS DEL MAR</h1>
 
-          <h2> CLINICA VETERINARIA</h2>
-          <h1>PUERTAS DEL MAR</h1>
-          
+              
+            </div>
         </div>
-      </div>
-    </div> 
+      </body>
+    </>
   )
 }
