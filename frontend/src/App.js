beb2b84252c2cea39d63bd1c3e5210c1adfd42b6
ImLuu <lmc018@alumnos.ucn.cl>
2022-09-20 00:10:03 -0300

    Comienzo Pantalla Inicio Sesion
    Pantalla basica de Iniciar sesion. incluye tarjeta, logo y nombre de la clinica

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import { PantallaInicio } from "./pages/Pantalla/PantallaInicio";
+//import { PantallaInicio } from "./pages/Pantalla/PantallaInicio";
+import {PantallaSesion} from "./pages/InicioSesion/PantallaSesion";
 
 function App() {
   return (
     <>
       <div>
         
-        <PantallaInicio/>
+        {/*<PantallaInicio/>*/}
+        <PantallaSesion/>
       </div>
     
     </>
---
+++ frontend/src/pages/InicioSesion/PantallaSesion.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../InicioSesion/PantallaSesion.css"
+
+export const PantallaSesion = () => {
+  return (
+    <>
+        <body>
+            <div className='barra'>
+                <div className='imagen-logo'></div>
+                    <div className='text-clinica'>CLINICA VETERINARIA</div>
+                    <div className='text-nombre'>PUERTAS DEL MAR</div>
+                    <div className='text-log'>Iniciar Sesión</div>
+            </div>
+
+            <div className='tarjeta'></div>
+        </body>
+        
+    </>
+  )
+}
