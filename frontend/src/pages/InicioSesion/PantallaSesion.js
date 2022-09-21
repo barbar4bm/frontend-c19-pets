37fed0b0a344c95bf3428c1f735e40b4d230baa3
ImLuu <lmc018@alumnos.ucn.cl>
2022-09-21 18:19:47 -0300

    Arreglos pagina inicio sesion
    se logro mejoras en el diseño de la pagina

--- frontend/src/pages/InicioSesion/PantallaSesion.js
+++ frontend/src/pages/InicioSesion/PantallaSesion.js
@@ -3,18 +3,26 @@ import "../InicioSesion/PantallaSesion.css"
 
 export const PantallaSesion = () => {
   return (
-    <>
-        <body>
-            <div className='barra'>
-                <div className='imagen-logo'></div>
-                    <div className='text-clinica'>CLINICA VETERINARIA</div>
-                    <div className='text-nombre'>PUERTAS DEL MAR</div>
-                    <div className='text-log'>Iniciar Sesión</div>
-            </div>
-
-            <div className='tarjeta'></div>
-        </body>
+    <>  
         
+        <body> 
+            
+            <div className='barra'>
+              <div className='imagen-logo'>
+                <div className='text-clinica'>Clinica veterinaria</div>
+                <div className='text-nombre'>Puertas del mar</div>
+                <div className='text-log'>iniciar sesión </div>
+                
+              </div>
+              
+            </div>
+            
+            <div className='tarjeta'></div>
+            
+            
+         
+        </body>
+       
     </>
   )
 }
