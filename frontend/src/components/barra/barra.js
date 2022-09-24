8fd08b83a82a00bbf46243de4f1679e0fe6d7ec0
scarlet-tobar <44346895+scarlet-tobar@users.noreply.github.com>
2022-09-24 14:47:13 -0300

    Frontend inicio sesión

--- frontend/src/components/barra/barra.js
+++ frontend/src/components/barra/barra.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import "./barra.css";
-
+import { Link } from 'react-router-dom';
 export const Barra = () => {
     return (
         <>
-            <button className='btn btn-secundary' style={{ float: 'right' }}>
-                Acerca de
-            </button>
+            < Link to="/">
+                <button className='btn btn-secundary' style={{ float: 'right' }}>
+                    Acerca de
+                </button>
+            </Link>
             <button className='btn btn-secundary' style={{ float: 'right' }}>
                 Agendar
             </button>
-            <button className='btn btn-secundary' style={{ float: 'right' }}>
-                Inicio Sesión
-            </button>
+            <Link to="/login" >
+                <button className='btn btn-secundary' style={{ float: 'right' }} Link to="/login">
+                    Inicio Sesión
+                </button>
+            </Link>
 
         </>
     )
--- frontend/src/pages/InicioSesion/PantallaSesion.js
+++ frontend/src/pages/InicioSesion/PantallaSesion.js
@@ -3,24 +3,45 @@ import "../InicioSesion/PantallaSesion.css"
 
 export const PantallaSesion = () => {
   return (
-    <>  
-        
-        <body> 
-            
-            <div className='barra'>
-              <div className='imagen-logo'>
-                <div className='text-clinica'>Clinica veterinaria</div>
-                <div className='text-nombre'>Puertas del mar</div>
-                <div className='text-log'>iniciar sesión </div>
-                
-              </div>      
+    <>
+
+      <body>
+
+        <div className='barra'>
+          <div className='imagen-logo'>
+            <div className='text-clinica'>Clinica veterinaria</div>
+            <div className='text-nombre'>Puertas del mar</div>
+            <div className='text-log'>iniciar sesión </div>
+
+          </div>
+        </div>
+        <div className='tarjeta'>
+          <div className='container'>
+            <div className="form-group mt-3">
+              <label>Email</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Ingrese email"
+              />
+              <label>Contraseña</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Ingrese contraseña"
+              />
+              <button type="submit" className="btn btn-primary" style={{ float: 'center' }} >
+                Iniciar sesión
+              </button>
             </div>
-            <div className='tarjeta'></div>
-            
-            
-         
-        </body>
-       
+          </div>
+        </div>
+
+
+
+
+      </body>
+
     </>
   )
 }
