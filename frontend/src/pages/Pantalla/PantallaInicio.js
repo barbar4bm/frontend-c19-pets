6443cb07983b7145114b8563f9141bc1d57c06da
ImLuu <lmc018@alumnos.ucn.cl>
2022-09-26 18:37:18 -0300

    detalles no agregados

--- frontend/src/pages/Pantalla/PantallaInicio.js
+++ frontend/src/pages/Pantalla/PantallaInicio.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import './PantallaInicio.css';
-
+import { Barra } from '../../components/barra/barra';
 
 export const PantallaInicio = () => {
 
   return (
    <>
-
-            <div className='fondo'>
+        
+            <div className='fondo'> 
+              <Barra/>
               <img src={require("../../components/icono.jpg")} 
                 alt="">
               </img>
               <h2> CLINICA VETERINARIA</h2>
               <h1>PUERTAS DEL MAR</h1>
             </div>
+    
+       
     </>
   )
 }
