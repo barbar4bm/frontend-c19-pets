13d27f480112ad663fa35b1f7d1a347eb5d695ba
ImLuu <lmc018@alumnos.ucn.cl>
2022-10-17 02:56:57 -0300

    pequeños cambios pantalla admin

--- frontend/src/pages/UsuarioInterfaz/UsuarioInterfaz.js
+++ frontend/src/pages/UsuarioInterfaz/UsuarioInterfaz.js
@@ -10,20 +10,25 @@ export const UsuarioInterfaz = () => {
         <div className='barra-header'>
           
           <div className='vet-logo'>
-            <div className='dash-titulo'>Dashboard</div>
+            <div className='dash-titulo'>Pantalla de Administración </div>
           </div>
 
           <div className='boton-salir'>
-            <button className='btn btn-secundary' style={{float:'right'}}>Cerrar</button>
+            
+              <button className='btn btn-secundary' style={{float:'right'}}>Cerrar</button>
+           
           </div>
          
-        </div>
+        </div>  
 
+        
+        <div className='user_profile'> ⭐ Usuario : Obtener info del usuario </div>
         <div className='sidebar'>
+        
           <ul>
             <li><a href='#'> 📅 Atenciones </a></li>
             <li><a href='#'> 🔎 Busqueda </a></li>
-            <li><a href='#'> ❌Eliminar Hora</a></li>
+            <li><a href='#'> ❌ Eliminar Hora</a></li>
           </ul>
 
          
@@ -31,8 +36,11 @@ export const UsuarioInterfaz = () => {
         <div className='midbar'>
             Aca debo mostrar la información
         </div>
-        
+        <div className='foot-page'>
+          <p className='text text-center' style={{color:'white'}}> Información de nosotros por cualquier tipo de problema </p>
+        </div>
       </div>
+      
       
       
     </>
